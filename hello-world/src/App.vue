c73b64c439dfcd7cd1c0750c3cc66ff0a7ec85f5
andutzu7 <rockkidd57@gmail.com>
2022-04-03 00:16:43 +0300

    layout 101

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,67 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+	<img alt="Vue logo" src="./assets/logo.png" />
 
-  <div>
-    <input v-model="text" />
-    <button v-on:click="handle_post()">Insert item</button>
-  </div>
-  <div v-for="(item, index) in items" :key="index">
-    {{ item.title }}
+<!--	<div>
+		<input v-model="text" />
+		<button v-on:click="handle_post()">Insert item</button>
+	</div>
+	<div v-for="(item, index) in items" :key="index">
+		{{ item.title }}
+		<button v-on:click="handle_delete(index)">Delete me!?!</button>
+	</div>
 
-    <button v-on:click="handle_delete(index)">Delete me!?!</button>
-  </div>
-</template>
+	!-->
+	<div id="app">
+	<notes/>
+	</div>
+	</template>
 
 <script>
 import axios from "axios"
+import Notes from './components/Items.vue';
 
+let default_routes={
+	"items":"http://localhost:8080/items",
+	"users":'http://localhost:8080/users',
+	"attributes":'http://localhost:8080/attributes',
+	"attribute_values":'http://localhost:8080/attributeValues',
+	"item_attribute_values":'http://localhost:8080/itemAttributeValues'
+};
 export default {
-  name: 'App',
-  components: {
-  },
-  methods:{
-	functional:function(){
-		axios.get('http://localhost:8080/items').then((result)=>{
-		this.items = result.data._embedded.items;
-		console.log(result.data)
+	name: 'App',
+	components: {Notes},
+	methods:{
+		functional:function(){
+			axios.get(default_routes["items"]).then((result)=>{
+				this.items = result.data._embedded.items;
+				console.log(result.data)
 
-	})
-	},
-	handle_delete:function(index){
-		let item_link = this.items[index]._links.self.href;
-		axios.delete(item_link).then(()=>{
-			this.items.splice(index,1)
-		})
-	},
+			})
+		},
+		handle_delete:function(index){
+			let item_link = this.items[index]._links.self.href;
+			axios.delete(item_link).then(()=>{
+				this.items.splice(index,1)
+			})
+		},
 
-	handle_post:function(){
-		let fields = {"user_id": 1,"title": this.text}
-		axios.post('http://localhost:8080/items',fields).then((result)=>{
-		let newItem = result.data;
-		this.items.push(newItem);
-		this.text='';
-		})
-	}
-	},
-  data: function(){
-	return {
-		items:[],
-		text:''
+		handle_post:function(){
+			let fields = {"user_id": 1,"title": this.text}
+			axios.post('http://localhost:8080/items',fields).then((result)=>{
+				let newItem = result.data;
+				this.items.push(newItem);
+				this.text='';
+			})
 		}
 	},
-  mounted: function(){
-	this.functional();
-}
+	data: function(){
+		return {
+			items:[],
+			text:'',
+		}
+	},
+	mounted: function(){
+		this.functional();
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
